ac6d79f2aafd1f5128a04deddfc4116b2b2837a3
Rafael de Souza <rafael.de.souza@outlook.com>
2019-06-16 13:34:15 -0300

    Update login buttons to mdc buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
 import {MDCTextField} from "@material/textfield/component";
+import {MDCRipple} from "@material/ripple/component";
 
 require('../css/app.scss');
 
 document.querySelectorAll('.mdc-text-field').forEach(function (element) {
     new MDCTextField(element);
+});
+
+document.querySelectorAll('.mdc-button').forEach(function (element) {
+    new MDCRipple(element);
 });
