590d1c30d95e5809db464adf0ca33c98dec9f87f
Rafael de Souza <rafael.de.souza@outlook.com>
2019-06-21 22:43:13 -0300

    Define base layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
+import {MDCDrawer} from "@material/drawer/component";
+import {MDCTopAppBar} from "@material/top-app-bar/component";
 import {MDCTextField} from "@material/textfield/component";
 import {MDCRipple} from "@material/ripple/component";
 
 require('../css/app.scss');
+
+let drawer;
+let element;
 
 document.querySelectorAll('.mdc-text-field').forEach(function (element) {
     new MDCTextField(element);
@@ -10,3 +15,20 @@ document.querySelectorAll('.mdc-text-field').forEach(function (element) {
 document.querySelectorAll('.mdc-button').forEach(function (element) {
     new MDCRipple(element);
 });
+
+element = document.querySelector('.mdc-drawer');
+
+if (element) {
+    drawer = new MDCDrawer(element);
+}
+
+element = document.querySelector('.mdc-top-app-bar');
+if (element) {
+    const topAppBar = new MDCTopAppBar(element);
+    topAppBar.setScrollTarget(document.getElementById('main-content'));
+    topAppBar.listen('MDCTopAppBar:nav', () => {
+        if (drawer) {
+            drawer.open = !drawer.open;
+        }
+    });
+}
