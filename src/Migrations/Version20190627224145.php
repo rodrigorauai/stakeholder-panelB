58650c77fc194c121d55846d4020fd67a92b5bca
Rafael de Souza <rafael.de.souza@outlook.com>
2019-06-27 19:43:16 -0300

    Fix payment property name from isRealized to wasMade

---
+++ src/Migrations/Version20190627224145.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190627224145 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Rename Payment\'s isRealized to wasMade';
+    }
+
+    /**
+     * @param Schema $schema
+     * @throws DBALException
+     */
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE account_financial_movement CHANGE is_realized was_made TINYINT(1) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     * @throws DBALException
+     */
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE account_financial_movement CHANGE was_made is_realized TINYINT(1) DEFAULT NULL');
+    }
+}
