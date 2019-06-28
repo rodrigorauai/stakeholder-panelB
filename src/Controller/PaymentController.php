8666d124052c358d7764379eb7f447e291ff49fa
Rafael de Souza <rafael.de.souza@outlook.com>
2019-06-28 02:57:11 -0300

    Add Payment's invoice route, logic and interface

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Payment;
+use App\Form\PaymentInvoiceType;
 use App\Repository\PaymentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,6 +24,31 @@ class PaymentController extends AbstractController
 
         return $this->render('payment/index.html.twig', [
             'payments' => $payments,
+        ]);
+    }
+
+    /**
+     * @param Payment $payment
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     * @Route("/pagamentos/{id}/nota-fiscal", name="payment__invoice__edit")
+     */
+    public function invoiceEdit(Payment $payment, Request $request, EntityManagerInterface $entityManager)
+    {
+        $form = $this->createForm(PaymentInvoiceType::class, $payment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($payment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('payment__invoice__edit', ['id' => $payment->getId()], 303);
+        }
+
+        return $this->render('payment/invoice/edit.html.twig', [
+            'payment' => $payment,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/PaymentInvoiceType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Payment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PaymentInvoiceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('invoiceUrl', UrlType::class, [
+                'label' => 'URL da Nota Fiscal',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Payment::class,
+        ]);
+    }
+}
