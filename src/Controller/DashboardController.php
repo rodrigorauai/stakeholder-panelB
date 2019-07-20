a003d467a62202d255e2d76117f983c2d16e2e8e
Rafael de Souza <rafael.de.souza@outlook.com>
2019-07-20 17:20:33 -0300

    Add total investment value to stakeholder's dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Helper\DashboardHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
     /**
+     * @param DashboardHelper $dashboardHelper
+     * @return Response
      * @Route("/dashboard", name="dashboard")
      */
-    public function index()
+    public function index(DashboardHelper $dashboardHelper)
     {
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => self::class,
+            'dashboardHelper' => $dashboardHelper,
         ]);
     }
 }
