da69c49b45cd50d45aece3e894c17e7b1ce6dbb2
Rafael de Souza <rafael.de.souza@outlook.com>
2019-07-20 20:08:33 -0300

    Fix total co-participation value query to exclude non-disclosed payments

--- src/Repository/PaymentRepository.php
+++ src/Repository/PaymentRepository.php
@@ -78,10 +78,13 @@ class PaymentRepository extends ServiceEntityRepository
         $qb = $this->createQueryBuilder('payment');
         $qb
             ->select('SUM(payment.value)')
-            ->where(
-                $qb->expr()->eq('payment.provenance', ':provenance')
-            )
+            ->leftJoin('payment.reward', 'reward')
+            ->where($qb->expr()->andX(
+                $qb->expr()->eq('payment.provenance', ':provenance'),
+                $qb->expr()->lte('reward.disclosureDate', ':now')
+            ))
             ->setParameter('provenance', Payment::PROVENANCE_CO_PARTICIPATION)
+            ->setParameter('now', new DateTime())
         ;
 
         if (count($accounts) > 0) {
