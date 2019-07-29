bfa2c81241d5d2d26bc7b00a98237a62aa4054f3
Matheus Reis <matheusreis@adinvest.com>
2019-07-29 10:37:58 -0300

    Administrator wihtout access to /dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Helper\DashboardHelper;
+use App\Helper\ProfileHelper;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +15,16 @@ class DashboardController extends AbstractController
      * @param DashboardHelper $dashboardHelper
      * @return Response
      * @Route("/dashboard", name="dashboard")
+     * @IsGranted({"ROLE_STAKEHOLDER"})
      */
-    public function index(DashboardHelper $dashboardHelper)
+    public function index(ProfileHelper $profileSwitcher, DashboardHelper $dashboardHelper)
     {
+        $currentProfile = $profileSwitcher->getCurrentProfile();
+        switch ($currentProfile['id']) {
+            case ProfileHelper::PROFILE_ADMINISTRATOR:
+                return $this->redirectToRoute('withdraw__index');
+        }
+
         return $this->render('dashboard/index.html.twig', [
             'dashboardHelper' => $dashboardHelper,
         ]);
