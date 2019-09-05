b2ddedd8af18c30a43c7ca528c1ffdf3afec44b4
Matheus Reis <matheusreis@adinvest.com>
2019-09-05 18:25:48 -0300

    Currency label in pessoas/{id}/contas-de-patrocinio

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -16,6 +16,7 @@ use App\Form\PersonType;
 use App\Helper\PasswordHelper;
 use App\Helper\UploadHelper;
 use App\Repository\PersonRepository;
+use App\Repository\ConfigurationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Psr\Log\LoggerInterface;
@@ -229,9 +230,12 @@ class PersonController extends AbstractController
      * @Route("/pessoas/{id}/contas-de-patrocinio", name="person_account__index")
      * @IsGranted({"ROLE_ADMINISTRATIVE_ASSISTANT"})
      */
-    public function showAccounts(Person $person)
+    public function showAccounts(Person $person, ConfigurationRepository $repository)
     {
+        $currency = $repository->findOneByActive();
+
         return $this->render('person/show--accounts.html.twig', [
+            'currency' => $currency->getLabel(),
             'person' => $person,
         ]);
     }
